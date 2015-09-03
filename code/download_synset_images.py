3ba72391655be55febbe4317572f8fa8b12517af
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-09-04 02:34:44 +1000

    Improve caching in download scripts

--- code/download_synset_images.py
+++ code/download_synset_images.py
@@ -7,6 +7,9 @@ import os.path
 import yaml
 import argparse
 from bs4 import BeautifulSoup as Soup
+import glob
+import random
+from concurrent import futures
 
 parser = argparse.ArgumentParser(description='ImageNet Gatherer')
 parser.add_argument('image_folder', type=str, nargs='?', default='images', help='The folder to store the downloaded images')
@@ -16,16 +19,19 @@ args = parser.parse_args()
 
 image_folder = args.image_folder
 
+def loadCache(cache_name):
+	cache = {}
+	cache_files = glob.glob("cache/{}__*.dat".format(cache_name))
+	for cache_file_name in cache_files:
+		with open(cache_file_name, 'r+') as cache_file:
+			words = map(lambda l: l.strip(), cache_file.readlines())
+			for word in words:
+				cache[word] = 0
+	return cache
 
-bad_image_cache_file = 'bad_image_cache.dat'
-bad_image_cache = {}
-if os.path.exists(bad_image_cache_file):
-	print 'LOADING BAD IMAGE CACHE:'
-	with open(bad_image_cache_file, 'r+') as cache:
-		words = map(lambda l: l.strip(), cache.readlines())
-		for word in words:
-			bad_image_cache[word] = 0
-		print '  size: {}'.format(len(bad_image_cache))
+print 'LOADING BAD IMAGE CACHE:'
+bad_image_cache = loadCache('bad_image_cache')
+print '  size: {}'.format(len(bad_image_cache))
 
 
 blacklisted_images_sha1_hashes = [
@@ -46,10 +52,13 @@ for parent_word in parent_words:
 	for child_word in hyponym_data_url.readlines()[1:]:  # ignore first line as its the 'parent word'
 		search_words.append(child_word[1:].strip())  # ignore proceeding dash and strip trailing newline
 
-url_map = {}
-
-for search_word in search_words:
+def downloadImagesForSearchWord(search_word):
 	print 'BEGIN SEARCH WORD: {}'.format(search_word)
+
+	bad_image_cache_file = 'cache/bad_image_cache__{}.dat'.format(random.randint(1000, 9999))
+	def addToBadImageCache(wordnet_name):
+		with open(bad_image_cache_file, 'a+') as cache:
+			cache.write("{}\n".format(wordnet_name))
 
 	mapping_data_url = urllib2.urlopen('http://www.image-net.org/api/text/imagenet.synset.geturls.getmapping?wnid={}'.format(search_word))
 	for map in mapping_data_url.readlines():
@@ -71,14 +80,27 @@ for search_word in search_words:
 				sha1hash = hashlib.sha1(image_data).hexdigest()
 				if str(sha1hash) in blacklisted_images_sha1_hashes:
 					print "Image blacklisted: {}".format(object_name)
+					addToBadImageCache(object_name)
 					continue
 				im = Image.open(io.BytesIO(image_data))
 				im.save(output_image_filename, "JPEG")
 				print "Saved to {}".format(output_image_filename)
 			except Exception as e:
 				print "Error retrieving for file {}: {}".format(object_name, e)
-				with open(bad_image_cache_file, 'a+') as cache:
-					cache.write("{}\n".format(object_name))
+				addToBadImageCache(object_name)
 				continue
 		else:
 			print 'Image already exists: {}'.format(object_name)
+
+with futures.ThreadPoolExecutor(max_workers=8) as executor:
+	# Build set of futures:
+	future_results = {}
+	for search_word in search_words:
+		future_results[search_word] = executor.submit(downloadImagesForSearchWord, search_word)
+
+	for future in futures.as_completed(future_results):
+		search_word = future_results[future]
+		if future.exception() is not None:
+			print 'THE SEARCH WORD {} GENERATED AN EXCEPTION: {}'.format(search_word, future.exception())
+		else:
+			print 'ALL IMAGES FOR SEARCH WORD {} DOWNLOADED.'.format(search_word)
--- code/download_synset_images_with_info.py
+++ code/download_synset_images_with_info.py
@@ -17,6 +17,34 @@ args = parser.parse_args()
 
 image_folder = args.image_folder
 
+bad_search_word_cache_file = 'bad_search_word_cache_dsiwi.dat'
+bad_search_word_cache = {}
+if os.path.exists(bad_search_word_cache_file):
+	print 'LOADING BAD SEARCH WORD CACHE:'
+	with open(bad_search_word_cache_file, 'r+') as cache:
+		words = map(lambda l: l.strip(), cache.readlines())
+		for word in words:
+			bad_search_word_cache[word] = 0
+		print '  size: {}'.format(len(bad_search_word_cache))
+
+def addToBadSearchWordCache(wordnet_name):
+	with open(bad_search_word_cache_file, 'a+') as cache:
+		cache.write("{}\n".format(wordnet_name))
+
+bad_image_cache_file = 'bad_image_cache_dsiwi.dat'
+bad_image_cache = {}
+if os.path.exists(bad_image_cache_file):
+	print 'LOADING BAD IMAGE CACHE:'
+	with open(bad_image_cache_file, 'r+') as cache:
+		words = map(lambda l: l.strip(), cache.readlines())
+		for word in words:
+			bad_image_cache[word] = 0
+		print '  size: {}'.format(len(bad_image_cache))
+
+def addToBadImageCache(wordnet_name):
+	with open(bad_image_cache_file, 'a+') as cache:
+		cache.write("{}\n".format(wordnet_name))
+
 blacklisted_images_sha1_hashes = [
 	'10f3f7f79e6528aa9d828316248997568ac0d833'  # flickr 'photo not available' image
 ]
@@ -33,7 +61,9 @@ file.close()
 
 search_words = parent_words[:]
 
+print 'DOWNLOADING PARENT WORD HYPONYMS:'
 for parent_word in parent_words:
+	print '  {}'.format(parent_word)
 	hyponym_data_url = urllib2.urlopen('http://www.image-net.org/api/text/wordnet.structure.hyponym?wnid={}&full=1'.format(parent_word))
 	for child_word in hyponym_data_url.readlines()[1:]:  # ignore first line as its the 'parent word'
 		search_words.append(child_word[1:].strip())  # ignore proceeding dash and strip trailing newline
@@ -50,9 +80,15 @@ with open(output_dat_filename, 'r') as dat_file:
 
 with open(output_dat_filename, 'a+') as dat_file:
 	for search_word in search_words:
+		print 'BEGIN SEARCH WORD: {}'.format(search_word)
+
 		if search_word in blacklisted_search_words:
 			print 'Blacklisted search word: {}'.format(search_word)
 			continue
+		if search_word in bad_search_word_cache:
+			print "Skipping cached bad search word {}.".format(search_word)
+			continue
+
 		mapping_data_url = urllib2.urlopen('http://www.image-net.org/api/text/imagenet.synset.geturls.getmapping?wnid={}'.format(search_word))
 		for map in mapping_data_url.readlines():
 			parts = map.strip().partition(' ')
@@ -66,6 +102,7 @@ with open(output_dat_filename, 'a+') as dat_file:
 			print 'Could not open bounding box URL: {}'.format(bounding_boxes_url)
 			print 'See: http://www.image-net.org/synset?wnid={}'.format(search_word)
 			print e
+			addToBadSearchWordCache(search_word)
 			continue
 
 		with tarfile.open(fileobj=bounding_boxes_url_data, mode='r|*') as bounding_boxes_file:
@@ -75,6 +112,10 @@ with open(output_dat_filename, 'a+') as dat_file:
 					xml = Soup(bounding_box_file)
 					objects = xml.findAll('object')
 					object_name = xml.annotation.filename.string
+
+					if object_name in bad_image_cache:
+						print "Skipping cached bad image {}.".format(object_name)
+						continue
 
 					output_image_filename = '{}/{}.jpg'.format(image_folder, object_name)
 					if output_image_filename in cached_bbox:
@@ -94,12 +135,14 @@ with open(output_dat_filename, 'a+') as dat_file:
 							sha1hash = hashlib.sha1(image_data).hexdigest()
 							if str(sha1hash) in blacklisted_images_sha1_hashes:
 								print "Image blacklisted: {}".format(object_name)
+								addToBadImageCache(object_name)
 								continue
 							im = Image.open(io.BytesIO(image_data))
 							im.save(output_image_filename, "JPEG")
 							print "Saved to {}".format(output_image_filename)
 						except Exception as e:
 							print "Error retrieving for file {}: {}".format(object_name, e)
+							addToBadImageCache(object_name)
 							continue
 					else:
 						print 'Image already exists: {}'.format(object_name)
